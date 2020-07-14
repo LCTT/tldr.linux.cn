d3e7beb9cc73ad0bff06cbe4fb6f9510080d9f74
白宦成 <best.tony@foxmail.com>
2020-07-14 14:35:09 +0800

    feat: add link

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <v-app-bar
       app
       color="primary"
-      
+
     >
       <v-toolbar-title class="display-2 font-weight-black">TLDR</v-toolbar-title>
 
@@ -13,7 +13,7 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn> -->
 
-     
+
     </v-app-bar>
 
     <v-content>
@@ -23,7 +23,7 @@
     </v-content>
     <v-footer>
     <v-spacer></v-spacer>
-      <div>&copy; <a href="https://linux.cn" target="_blank">Linux.cn</a> {{ new Date().getFullYear() }}</div>
+      <div>&copy; <a href="https://linux.cn" target="_blank">Linux.cn</a> {{ new Date().getFullYear() }} & <a href="https://cloud.tencent.com/product/wh?from=12331">Hosted BY 云开发 Cloudbase </a></div>
     </v-footer>
   </v-app>
 </template>
@@ -41,7 +41,7 @@ export default {
   }),
 
   created: function(){
-    
+
   }
 };
 </script>
