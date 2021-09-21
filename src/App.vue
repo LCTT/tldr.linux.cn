c92f4046ce30280e40f2bb0bd05c19d984bdce90
bestony <xiqingongzi@gmail.com>
2021-09-21 19:28:11 +0800

    feat: header text goto home
    Author:  HuanCheng Bai <bestony@linux.com>

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,11 @@
       color="primary"
 
     >
-      <v-toolbar-title class="display-2 font-weight-black">TLDR</v-toolbar-title>
+      <v-toolbar-title @click="goHome()" class="display-2 font-weight-black">TLDR</v-toolbar-title>
 
 
       <v-spacer></v-spacer>
-     <!--  <v-btn icon>
-        <v-icon>mdi-magnify</v-icon>
-      </v-btn> -->
-
-
+    
     </v-app-bar>
 
     <v-content>
@@ -23,7 +19,7 @@
     </v-content>
     <v-footer>
     <v-spacer></v-spacer>
-      <div>&copy; <a href="https://linux.cn" target="_blank">Linux.cn</a> {{ new Date().getFullYear() }} & <a href="https://cloud.tencent.com/product/wh?from=12331">Hosted BY 云开发 Cloudbase </a></div>
+      <div>&copy; <a href="https://linux.cn" target="_blank">Linux.cn</a> {{ new Date().getFullYear() }} </div>
     </v-footer>
   </v-app>
 </template>
@@ -42,6 +38,11 @@ export default {
 
   created: function(){
 
+  },
+  methods:{
+    goHome(){
+    this.$router.replace("/")
+  }
   }
 };
 </script>
