ac63ed5432f24b7fb600551b6fa0f869329ddb05
bestony <xiqingongzi@gmail.com>
2021-09-21 19:38:56 +0800

    fix: replace in home page warn
    Author:  HuanCheng Bai <bestony@linux.com>

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,11 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-
-    >
-      <v-toolbar-title @click="goHome()" class="display-2 font-weight-black">TLDR</v-toolbar-title>
-
+    <v-app-bar app color="primary">
+      <v-toolbar-title @click="goHome()" class="display-2 font-weight-black"
+        >TLDR</v-toolbar-title
+      >
 
       <v-spacer></v-spacer>
-    
     </v-app-bar>
 
     <v-content>
@@ -18,31 +14,33 @@
       </transition>
     </v-content>
     <v-footer>
-    <v-spacer></v-spacer>
-      <div>&copy; <a href="https://linux.cn" target="_blank">Linux.cn</a> {{ new Date().getFullYear() }} </div>
+      <v-spacer></v-spacer>
+      <div>
+        &copy; <a href="https://linux.cn" target="_blank">Linux.cn</a>
+        {{ new Date().getFullYear() }}
+      </div>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
-  name: 'App',
+  name: "App",
 
-  components: {
-  },
+  components: {},
 
   data: () => ({
     //
   }),
 
-  created: function(){
-
+  created: function() {},
+  methods: {
+    goHome() {
+      if(this.$router.currentRoute.fullPath === "/"){
+        return
+      }
+      this.$router.replace("/");
+    },
   },
-  methods:{
-    goHome(){
-    this.$router.replace("/")
-  }
-  }
 };
 </script>
